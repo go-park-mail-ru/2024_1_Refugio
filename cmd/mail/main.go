908833f9a16fcc88e710fe4474a8d5652abc568c
Иван Карпов <ivankarpov@MacBook-Pro-Ivan.local>
2024-04-07 00:50:57 +0300

    Fix

--- cmd/mail/main.go
+++ cmd/mail/main.go
@@ -37,7 +37,7 @@ import (
 // @BasePath /
 func main() {
 	// dsn := "user=postgres dbname=Mail password=postgres host=localhost port=5432 sslmode=disable"
-	dsn := "user=postgres dbname=Mail password=postgres host=0.0.0.0 port=5432 sslmode=disable"
+	dsn := "user=postgres dbname=Mail password=postgres host=89.208.223.140 port=5432 sslmode=disable"
 	// dsn := "user=postgres dbname=Mail password=postgres host=db port=5432 sslmode=disable"
 	db, errDb := sql.Open("pgx", dsn)
 	if errDb != nil {
