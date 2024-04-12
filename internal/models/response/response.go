b4acc473564fd6648c698561a751dfbd22b48f97
Иван Карпов <ivankarpov@MacBook-Pro-Ivan.local>
2024-04-12 17:37:05 +0300

    Fix

--- internal/models/response/response.go
+++ internal/models/response/response.go
@@ -39,21 +39,19 @@ func HandleError(w http.ResponseWriter, status int, message string) {
 	json.NewEncoder(w).Encode(response)
 }
 
-type UserGender string
-
 // UserSwag represents information about a user.
 type UserSwag struct {
-	ID          uint32     `json:"id,omitempty"`          // ID uniquely identifies the user.
-	FirstName   string     `json:"firstname,omitempty"`   // FirstName stores the first name of the user.
-	Surname     string     `json:"surname,omitempty"`     // Surname stores the last name of the user.
-	Patronymic  string     `json:"middlename,omitempty"`  // Patronymic stores the middle name of the user, if available.
-	Gender      UserGender `json:"gender,omitempty"`      // Gender stores the gender of the user.
-	Birthday    time.Time  `json:"birthday,omitempty"`    // Birthday stores the birthdate of the user.
-	Login       string     `json:"login"`                 // Login is the username used for authentication.
-	Password    string     `json:"password"`              // Password is the hashed password of the user.
-	AvatarID    string     `json:"avatar,omitempty"`      // AvatarID stores the identifier of the user's avatar image.
-	PhoneNumber string     `json:"phonenumber,omitempty"` // PhoneNumber stores the phone number of the user.
-	Description string     `json:"description,omitempty"` // Description stores additional information about the user.
+	ID          uint32         `json:"id,omitempty"`          // ID uniquely identifies the user.
+	FirstName   string         `json:"firstname,omitempty"`   // FirstName stores the first name of the user.
+	Surname     string         `json:"surname,omitempty"`     // Surname stores the last name of the user.
+	Patronymic  string         `json:"middlename,omitempty"`  // Patronymic stores the middle name of the user, if available.
+	Gender      UserGenderSwag `json:"gender,omitempty"`      // Gender stores the gender of the user.
+	Birthday    time.Time      `json:"birthday,omitempty"`    // Birthday stores the birthdate of the user.
+	Login       string         `json:"login"`                 // Login is the username used for authentication.
+	Password    string         `json:"password"`              // Password is the hashed password of the user.
+	AvatarID    string         `json:"avatar,omitempty"`      // AvatarID stores the identifier of the user's avatar image.
+	PhoneNumber string         `json:"phonenumber,omitempty"` // PhoneNumber stores the phone number of the user.
+	Description string         `json:"description,omitempty"` // Description stores additional information about the user.
 }
 
 // EmailSwag represents information about a email.
@@ -71,3 +69,11 @@ type EmailSwag struct {
 	SenderEmail    string    `json:"senderEmail"`              // SenderEmail is the Email of the sender user
 	RecipientEmail string    `json:"recipientEmail"`           // RecipientEmail is the Email of the recipient user
 }
+
+type UserGenderSwag string
+
+const (
+	Male   UserGenderSwag = "Male"
+	Female UserGenderSwag = "Female"
+	Other  UserGenderSwag = "Other"
+)
