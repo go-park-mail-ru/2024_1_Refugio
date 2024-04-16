97b3e1311bef1b5066ac3d1c7e2ec653c873e8b4
Иван Карпов <ivankarpov@MacBook-Pro-Ivan.local>
2024-04-16 22:39:56 +0300

    Fix cors

--- smtp_server/main.go
+++ smtp_server/main.go
@@ -14,7 +14,7 @@ import (
 	"regexp"
 )
 
-const sendEmailEndpoint = "http://89.208.223.140:8080/api/v1/auth/sendOther"
+const sendEmailEndpoint = "https://mailhub.su/api/v1/auth/sendOther"
 
 func main() {
 	serverAddr := "0.0.0.0:587"
