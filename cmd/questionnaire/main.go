d06dad5ef561c49878c0020dc8b82e13068140bb
Иван Карпов <ivankarpov@MacBook-Pro-Ivan.local>
2024-04-27 14:58:32 +0300

    Fix

--- cmd/questionnaire/main.go
+++ cmd/questionnaire/main.go
@@ -45,7 +45,7 @@ func settingTime() {
 
 func initializeDatabase() *sql.DB {
 	// dsn := "user=postgres dbname=Question password=postgres host=localhost port=5432 sslmode=disable"
-	dsn := "user=postgres dbname=Mail password=postgres host=89.208.223.140 port=5433 sslmode=disable"
+	dsn := "user=postgres dbname=Question password=postgres host=89.208.223.140 port=5433 sslmode=disable"
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		log.Fatalln("Can't parse config", err)
