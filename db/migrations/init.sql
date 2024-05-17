bd42070d27a0ef524bbb53e2a58779e59cfbbc75
Fedasov <fedasov03@inbox.ru>
2024-05-18 00:10:50 +0300

    fix

--- db/migrations/init.sql
+++ db/migrations/init.sql
@@ -62,8 +62,8 @@ CREATE TABLE IF NOT EXISTS profile_email (
     profile_id INT,
     email_id INT,
     PRIMARY KEY ( profile_id, email_id ),
-    CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES profile(id),
-    CONSTRAINT fk_email FOREIGN KEY (email_id)  REFERENCES email(id)
+    CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES profile(id) ON DELETE CASCADE,
+    CONSTRAINT fk_email FOREIGN KEY (email_id)  REFERENCES email(id) ON DELETE CASCADE
 );
 
 -- Создание таблицы папок (folder)
