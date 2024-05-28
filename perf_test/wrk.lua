c02bd2f898992954074736d5970821bb80fbdb4f
Fedasov <fedasov03@inbox.ru>
2024-05-28 23:25:51 +0300

    adsaf

--- perf_test/wrk.lua
+++ perf_test/wrk.lua
@@ -3,16 +3,7 @@ local wrk = require "wrk"
 
 wrk.method = "POST"
 wrk.body = '{"firstKey": "somedata", "secondKey": "somedata"}'
-wrk.body = '{
-    "topic": "WRK",
-    "text": "WRK_Test",
-    "readStatus": false,
-    "mark": false,
-    "replyToEmailId": 0,
-    "draftStatus": false,
-    "spamStatus": false,
-    "senderEmail": "wrk@mailhub.su",
-    "recipientEmail": "ivan@mailhub.su"}'
+wrk.body = '{"topic": "WRK","text": "WRK_Test","readStatus": false,"mark": false,"replyToEmailId": 0,"draftStatus": false,"spamStatus": false,"senderEmail": "wrk@mailhub.su","recipientEmail": "ivan@mailhub.su"}'
 wrk.headers["Content-Type"] = "application/json"
 wrk.headers["X-Csrf-Token"] = "e5bf01a0faf9177e583c49b0db26e10f"
 wrk.headers["session_id"] = "4ec9f9423c727e9e701ad2f2357503b7"
